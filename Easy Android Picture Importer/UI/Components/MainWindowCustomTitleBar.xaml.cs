6da629331f6a242c0123653a7e1906e478fd668d
CodingSeb <sebgeiser@gmail.com>
2024-06-20 07:55:33 +0200

    Back to systemButton by os management

--- Easy Android Picture Importer/UI/Components/MainWindowCustomTitleBar.xaml.cs
+++ Easy Android Picture Importer/UI/Components/MainWindowCustomTitleBar.xaml.cs
@@ -18,6 +18,8 @@ public partial class MainWindowCustomTitleBar : UserControl
     }
 
     private const int HTMAXBUTTON = 9;
+    private const int HTMINBUTTON = 8;
+    private const int HTCLOSE = 20;
     private IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
     {
         var message = (User32.WM)msg;
@@ -29,9 +31,11 @@ public partial class MainWindowCustomTitleBar : UserControl
                 {
                     int x = lparam.ToInt32() & 0xffff;
                     int y = lparam.ToInt32() >> 16;
+                    
                     var rect = new Rect(MaximizeWindowButton.PointToScreen(
                         new Point()),
                         new Size(MaximizeWindowButton.ActualWidth, MaximizeWindowButton.ActualWidth));
+
                     if (rect.Contains(new Point(x, y)))
                     {
                         MaximizeWindowButton.SpecialIsHover = true;
@@ -41,6 +45,36 @@ public partial class MainWindowCustomTitleBar : UserControl
                     else
                     {
                         MaximizeWindowButton.SpecialIsHover = false;
+                    }
+
+                    rect = new Rect(MinimizeButton.PointToScreen(
+                        new Point()),
+                        new Size(MinimizeButton.ActualWidth, MinimizeButton.ActualWidth));
+
+                    if (rect.Contains(new Point(x, y)))
+                    {
+                        MinimizeButton.SpecialIsHover = true;
+                        handled = true;
+                        return new IntPtr(HTMINBUTTON);
+                    }
+                    else
+                    {
+                        MinimizeButton.SpecialIsHover = false;
+                    }
+
+                    rect = new Rect(ExitButton.PointToScreen(
+                        new Point()),
+                        new Size(ExitButton.ActualWidth, ExitButton.ActualWidth));
+
+                    if (rect.Contains(new Point(x, y)))
+                    {
+                        ExitButton.SpecialIsHover = true;
+                        handled = true;
+                        return new IntPtr(HTCLOSE);
+                    }
+                    else
+                    {
+                        ExitButton.SpecialIsHover = false;
                     }
                 }
                 catch (OverflowException)
